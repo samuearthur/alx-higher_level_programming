c773b8552f2bb84c3f1f410dc15b39276a65d9ca
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-10-18 20:39:35 +0000

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const movie_id = process.argv[2];
-
-if (parseInt(movie_id) < 8) {
-    const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
-    request(url, function (err, response, body) {
-	if (err) {
-	    return console.log(err);
-	}
-	console.log(JSON.parse(body).title);
-    });
-}
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
